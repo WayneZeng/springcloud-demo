b87a10771584c42eda14b434d850155f91ea58d9
yanuo <yanuo@wacai.com>
2019-04-08 23:00:06 +0800

    feat: add chaos demo

---
+++ chaosmonkey/src/main/java/com/demo/chaos/ChaosApplication.java
@@ -0,0 +1,12 @@
+package com.demo.chaos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaosApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChaosApplication.class, args);
+  }
+}
